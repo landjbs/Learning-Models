2f96f222753c0b8c6ac5afeabf796a65dc4c1798
landjbs <landonsmith@college.harvard.edu>
2019-03-16 14:41:27 -0400

    tests superimposition

--- image_test.py
+++ image_test.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 landon = plt.imread("landon.jpg")
 lorna = plt.imread("lorna.jpg")
 
-plt.imshow(landon[200:900,200:900])
+plt.imshow(landon[200:900,200:1000])
 plt.show()
