7edb054fd3eb8268a8381ec82149f90a976aa1b5
landjbs <landonsmith@college.harvard.edu>
2019-03-16 13:51:40 -0400

    updates to read in

--- image_analysis.py
+++ image_analysis.py
@@ -71,24 +71,21 @@ while True:
 
 	process_this_frame = not process_this_frame
 
-	# Display the results
-	for (top, right, bottom, left), name in zip(face_locations, face_names):
-		# Scale back up face locations since the frame we detected in was scaled to 1/4 size
-		top *= 4
-		right *= 4
-		bottom *= 4
-		left *= 4
+	if len(face_names) > 1:
 
-		if name == "Landon":
-			frame[top:bottom, left:right, 0] *= 4
-		elif name == "Lorna":
-			frame[top:bottom, left:right, 1] *= 4
+		# Display the results
+		for (top, right, bottom, left), name in zip(face_locations, face_names):
+			# Scale back up face locations since the frame we detected in was scaled to 1/4 size
+			top *= 4
+			right *= 4
+			bottom *= 4
+			left *= 4
 
-		cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+			cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-		cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-		font = cv2.FONT_HERSHEY_DUPLEX
-		cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+			cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+			font = cv2.FONT_HERSHEY_DUPLEX
+			cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
 	# Display the resulting image
 	cv2.imshow('Video', frame)
---
+++ image_test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+landon = plt.imread("landon.jpg")
+lorna = plt.imread("lorna.jpg")
+
+plt.imshow(landon[200:900,200:900])
+plt.show()
