c97c6f7d0a0943c5de0dbd99005ed583d28cf2c7
landjbs <landonsmith@college.harvard.edu>
2019-03-16 14:00:55 -0400

    1 2 face

--- image_analysis.py
+++ image_analysis.py
@@ -71,21 +71,30 @@ while True:
 
 	process_this_frame = not process_this_frame
 
-	if len(face_names) > 1:
+	if len(face_names) == 1:
+		locs1 = 4* face_locationss[0]
 
-		# Display the results
-		for (top, right, bottom, left), name in zip(face_locations, face_names):
-			# Scale back up face locations since the frame we detected in was scaled to 1/4 size
-			top *= 4
-			right *= 4
-			bottom *= 4
-			left *= 4
+	elif len(face_names) == 2:
 
-			cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+		locs1 = 4*(face_locations[0])
+		locs2 = 4*(face_locations[1])
 
-			cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-			font = cv2.FONT_HERSHEY_DUPLEX
-			cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+		face1 = frame[locs1[0]:locs1[2], locs1[4]:locs1[3]]
+		plt.imshow(face1)
+		plt.show()
+		#
+		# for (top, right, bottom, left), name in zip(face_locations, face_names):
+		# 	# Scale back up face locations since the frame we detected in was scaled to 1/4 size
+		# 	top *= 4
+		# 	right *= 4
+		# 	bottom *= 4
+		# 	left *= 4
+		#
+		# 	cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+		#
+		# 	cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+		# 	font = cv2.FONT_HERSHEY_DUPLEX
+		# 	cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
 	# Display the resulting image
 	cv2.imshow('Video', frame)
